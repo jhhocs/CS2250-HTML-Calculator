a3b85d9702615acc714f96b4785fb2c9b2ec972f
jhhocs <joshuaho1@cpp.edu>
2023-10-01 02:31:34 -0700

    calculator without enum

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const operations = document.querySelectorAll("button.operation")
 const equalsButton = document.getElementById("equals")
 const clearButton = document.getElementById("clear")
 
-var currentOperation;
-var firstNum = "0";
+var validOperation = true;
 var error = false;
 
 // Add event listener for numbers
@@ -22,43 +21,109 @@ function numberClicked(event) {
     if(error) return
 
     // Clear input box if an operation is selected
-    if(firstNum == inputBox.value) {
+    if(inputBox.value == '0') {
         inputBox.value = "";
     }
+    validOperation = true;
     inputBox.value += event.currentTarget.innerText;
 }
 
 function operationClicked(event) {
-    if(error) return
+    if(error || !validOperation) return
 
-    firstNum = inputBox.value
-    currentOperation = event.currentTarget.id;
+    validOperation = false;
+    inputBox.value += event.currentTarget.innerText;
 }
 
 equalsButton.addEventListener("click", (event) => {
     if(error) return
 
-    if(inputBox.value == "") {
-        // FIX MULTIPLE = ISSUE
-    }
-
-    let secondNumber = parseFloat(inputBox.value)
-    if(secondNumber == 0) {
+    if(isNaN(parseInt(inputBox.value.charAt(inputBox.value.length-1)))) {
         inputBox.value = "ERROR"
         error = true
         return
     }
-    console.log(firstNum + " " + secondNumber)
-    firstNum, inputBox.value = eval(parseFloat(firstNum) + currentOperation +  secondNumber);
+    let answer = computeInput(inputBox.value.split(/(?=[+\*/-])|(?<=[+\*/-])/g))
+
+    inputBox.value = answer
+
+    if(answer == "ERROR") {
+        error = true
+    }
 })
 
 clearButton.addEventListener("click", () => {
     clear()
 })
 
+function computeInput(input) {
+    console.log(input)
+    if(input.length == 1) {
+        return input[0]
+    }
+    if(input[0] == '-') {
+        input.splice(0, 2, `-${input[1]}`)
+    }
+    let operation = 0, operationIndex = 0
+    for(let i = 0; i < input.length; i++) {
+        switch(input[i]) {
+            case '+':
+                if(operation == 0) {
+                    operation = 1
+                    operationIndex = i
+                }
+
+                break
+            case '-':
+                if(operation == 0) {
+                    operation = 2
+                    operationIndex = i
+                }
+
+                break
+            case '*':
+                input.splice(i - 1, 3, multiply(parseInt(input[i-1]), parseInt(input[i+1])))
+
+                return computeInput(input)
+            case '/':
+                input.splice(i - 1, 3, divide(parseInt(input[i-1]), parseInt(input[i+1])))
+
+                return computeInput(input)
+            default:
+
+        }
+    }
+    if(operation == 1) {
+        input.splice(operationIndex - 1, 3, add(parseInt(input[operationIndex-1]), parseInt(input[operationIndex+1])))
+        return computeInput(input)
+    }
+    else if(operation == 2){
+        input.splice(operationIndex - 1, 3, subtract(parseInt(input[operationIndex-1]), parseInt(input[operationIndex+1])))
+        return computeInput(input)
+    }
+
+    // Uh oh
+    return -1
+}
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+function divide(num1, num2) {
+    return num1 / num2
+}
+
+function add(num1, num2) {
+    return num1 + num2
+}
+
+function subtract(num1, num2) {
+    return num1 - num2
+}
+
 function clear() {
     error = false
-    firstNum = "0"
     inputBox.value = "0"
-    secondNumber = null
+    validOperation = true;
 }
